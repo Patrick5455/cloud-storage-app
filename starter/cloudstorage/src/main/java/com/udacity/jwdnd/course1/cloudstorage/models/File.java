49e1d26794e4cd0de8f4b0fc4c745b2ad3b66e5c
patrick ojunde <patrickojunde@gmail.com>
2021-04-12 15:28:55 +0100

    added models

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/File.java
@@ -0,0 +1,10 @@
+package com.udacity.jwdnd.course1.cloudstorage.models;
+
+public class File {
+    private int fileId;
+    private String fileName;
+    private String contentType;
+    private Integer fileSize;
+    private int userId;
+    private Object fileData;
+}
---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/User.java
@@ -0,0 +1,11 @@
+package com.udacity.jwdnd.course1.cloudstorage.models;
+
+public class User {
+    private int userId;
+    private String userName;
+    private String salt;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+}
