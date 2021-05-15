c543c2224575a30a5fcd85ebed733e776169ee7c
patrick ojunde <patrickojunde@gmail.com>
2021-05-15 19:01:26 +0100

    updates on user validation

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/User.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/User.java
@@ -1,16 +1,28 @@
 package com.udacity.jwdnd.course1.cloudstorage.models;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Data
 public class User {
     private int userId;
+    @NotBlank(message = "username should not be empty")
     private String username;
+    @NotBlank(message = "firstname should not be empty")
     private String firstName;
+    @NotBlank(message = "lastname should not be blank")
     private String lastName;
     private String salt;
+    @Pattern(regexp = "\\A(?=\\S*?[0-9])(?=\\S*?[a-z])(?=\\S*?[A-Z])(?=\\S*?[@#$%^&+=])\\S{8,}\\z",
+            message = "password must be at least 8 characters in size." +
+            "\nAnd contain at least one of digits, upper and lower case, a special character like ^\nNo whitespace is allowed also")
     private String password;
     private String hashedPassword;
 
