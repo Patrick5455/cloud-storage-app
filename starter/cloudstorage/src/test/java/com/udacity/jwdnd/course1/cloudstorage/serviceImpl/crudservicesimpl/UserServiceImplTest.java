a3375a3c9721f092e4ed375ef79951d82607d807
patrick ojunde <patrickojunde@gmail.com>
2021-05-20 03:25:27 +0100

    added unit tests

---
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/serviceImpl/crudservicesimpl/UserServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.udacity.jwdnd.course1.cloudstorage.serviceImpl.crudservicesimpl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void userNameExists() {
+    }
+
+    @Test
+    void getUserByUserName() {
+    }
+}
