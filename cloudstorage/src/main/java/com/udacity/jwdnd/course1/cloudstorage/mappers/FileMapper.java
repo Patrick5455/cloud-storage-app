06625514b7a7220f83f312fc5614b14e4286c84f
patrick ojunde <patrickojunde@gmail.com>
2021-06-01 07:07:25 +0100

    updateed file CRUD DAO

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/FileMapper.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/FileMapper.java
@@ -1,8 +1,24 @@
 package com.udacity.jwdnd.course1.cloudstorage.mappers;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.udacity.jwdnd.course1.cloudstorage.models.File;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
-public class FileMapper {
+public interface FileMapper {
 
+    @Insert("INSERT INTO FILES (filename, contenttype, filesize, userid, filedata)" +
+            "VALUES(#{fileName}, #{contentType}, #{fileSize}, #{userId}, #{fileData})")
+    @Options(useGeneratedKeys = true, keyProperty = "fileId")
+    public int insertFile(File file);
+
+    @Select("SELECT * FROM FILES WHERE filename=#{fileName} AND userid=#{userId}")
+    public File getFileByFileName(int userId, String fileName);
+
+    @Select("SELECT * FROM FILES WHERE userid=#{userId}")
+    public List<File> getAllFiles(int userId);
+
+    @Delete("DELETE * FROM FILES WHERE filename=#{fileName} AND userid=#{userId}")
+    public void deleteFile(int fileName, int userId);
 }
--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/File.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/File.java
@@ -1,13 +1,22 @@
 package com.udacity.jwdnd.course1.cloudstorage.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class File {
+
     private int fileId;
+    @NotBlank(message = "file name must be specified")
     private String fileName;
+    @NotBlank(message = "file content type must be specified")
     private String contentType;
     private Integer fileSize;
     private int userId;
-    private Object fileData;
+    private byte[] fileData;
 }
